0447238ee390147964d7773227d5143a3ae8ddba
edumoro412 <edumoro2003@gmail.com>
2025-11-03 15:22:08 +0100

    feat(controller): cors origin added

--- src/main/java/com/example/urlshortener/controllers/UrlController.java
+++ src/main/java/com/example/urlshortener/controllers/UrlController.java
@@ -14,9 +14,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.CorsOrigin;
 
 
 
+@CorsOrigin(origins = "*")
 @RestController //Indica que esta clase expone endpoints REST (HTTP( GET, POST, etc.))
 @RequestMapping("/api") //todas las rutas comienzan con /api
 public class UrlController {
