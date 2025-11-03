17f416986bb215a967d08ad9a557cfe152c8b6d7
edumoro412 <edumoro2003@gmail.com>
2025-11-03 16:35:03 +0100

    feat(controller): fixed?

--- src/main/java/com/example/urlshortener/controllers/UrlController.java
+++ src/main/java/com/example/urlshortener/controllers/UrlController.java
@@ -14,11 +14,11 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.CorsOrigin;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 
 
-@CorsOrigin(origins = "*")
+@CrossOrigin(origins = "*")
 @RestController //Indica que esta clase expone endpoints REST (HTTP( GET, POST, etc.))
 @RequestMapping("/api") //todas las rutas comienzan con /api
 public class UrlController {
